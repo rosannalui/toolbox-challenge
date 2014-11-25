002be65f9bd406fe3f5380ea73f004eb9cc0a714
rosannalui <rosanna.lui123@gmail.com>
2014-11-24 19:30:57 -0800

    Fixed 3 clicks

--- js/app.js
+++ js/app.js
@@ -1,105 +1,118 @@
-"use strict"; 
-//create an array with all of the image numbers
-var imageNum =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]; 
+"use strict";
+
+//if user hits three card only show two cards and the third wone should remain unflipped 
+
+//the user should be able to play the game when the page loads
+//when the user has 8 matches alert the user with a YAY 
+//the user would have the option to click play again to play again or reset game. 
+
+	var matches = 0; 
 
 
-//shuffle the array then pull first 8 numbers twice into an array 
-var shuffledArray = _.shuffle(imageNum); 
-var firstEight = _.first(shuffledArray, 8);
-var newArray = []; 
-	for(var i =0; i <firstEight.length; i++) {
-		newArray.push(firstEight[i]); 
-		newArray.push(firstEight[i]); 
-} 
-//shuffle the order of the array
-var finalShuffle = _.shuffle(newArray); 
-console.log(finalShuffle);
+//sets up the game board 
+function onReady (){
+	//create an array with all of the image numbers
+	var imageNum =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]; 
+	//shuffle the array then pull first 8 numbers twice into an array 
+	var shuffledArray = _.shuffle(imageNum); 
+	var firstEight = _.first(shuffledArray, 8);
+	var newArray = []; 
+		for(var i = 0; i <firstEight.length; i++) {
+			newArray.push(firstEight[i]); 
+			newArray.push(firstEight[i]); 
+	} 
+	//shuffle the order of the array
+	var finalShuffle = _.shuffle(newArray); 
+	console.log(finalShuffle);
 
-//Loop over an array of filenames, and create an image for them, packing into an array:
-for (var i = 0; i < finalShuffle.length; i++) {
-    var image = new Image;
-    image.src = 'img/tile' + finalShuffle[i] + '.jpg'; //Set the src attribute (imageFiles[i] is the current filename in the loop)
-}
-
-//for loop that creates game board with back images. 
-//append images to the div run through a for loop ; 
-//function to build 4x4 //make a button to start game 
-//Show the result:
-
-var firstCard;
-var secondCard;
-var matches=0;  
-var clicks = 1;
-var storeImage;
-var storeSecond; 
-var blankSlate = $("#background1");
-//before the user clicks play 
-	for(var i = 0; i <finalShuffle.length; i++) {
-		var storeData = 'img/tile' + finalShuffle[i] + '.jpg';
-		var image =  $(document.createElement("img"));
-		image.data('reverseCard',storeData); 
-		image.attr("src","img/tile-back.png"); 
-		image.attr("style","width:200px"); 
-		blankSlate.append(image);
-		//console.log(image.data('reverseCard'));
+	//Loop over an array of filenames, and create an image for them, packing into an array:
+	for (var i = 0; i < finalShuffle.length; i++) {
+	    var image = new Image;
+	    image.src = 'img/tile' + finalShuffle[i] + '.jpg'; //Set the src attribute (imageFiles[i] is the current filename in the loop)
 	}
-$('#background1 img').click(function() {
-	console.log($(this).data('reverseCard')); 
-	//image.attr("src","img/tile-back.png"); 
-	$(this).attr("src", $(this).data('reverseCard'));
-	console.log(clicks);   
 
-	if(clicks==1) {
-		firstCard = $(this).data('reverseCard');
-		console.log("this works"+ clicks);
-		storeImage = $(this); 
-	//if the user clicks 2 cards check if the cards are the same 
-	 } else if (clicks==2) {
-			console.log("Clicks = 2"); 
-			secondCard = $(this).data('reverseCard');
-			storeSecond = $(this); 
-				if(firstCard==secondCard) {
-					matches++; 
-				} else {
-					//create a timer with a pause; 
-					setTimeout(function(){
-						storeImage.attr("src","img/tile-back.png");
-						storeSecond.attr("src","img/tile-back.png");
-						//$(this).attr("src","img/tile-back.png");
-					}, 1000);
-				}
-			//if firstCard == secondCard then do something, else reset back
-			clicks=0; 
+	var firstCard;
+	var secondCard;
+	var extraCardClick; 
+ 
+	var clicks = 1;
+	var storeImage;
+	var storeExtra; 
+	var totalTries = 0; 
+	var storeSecond; 
+	var blankSlate = $("#background1");
+	//before the user clicks play 
+		for(var i = 0; i <finalShuffle.length; i++) {
+			var storeData = "img/tile" + finalShuffle[i] + ".jpg";
+			var image =  $(document.createElement("img"));
+			image.data('reverseCard',storeData); 
+			image.attr("src","img/tile-back.png"); 
+			image.attr("style","width:200px"); 
+			blankSlate.append(image);
+		}
+	$('#background1 img').on('click', clicky);
+
+	function clicky() {
+		
+
+		if(clicks==1) {
+			$(this).attr("src", $(this).data('reverseCard')); 
+			firstCard = $(this).data('reverseCard');
+			storeImage = $(this); 
+			$(this).off('click');	
+			//console.log("first click");
+			clicks++;
+
+		//if the user clicks 2 cards check if the cards are the same 
+		 } else if(clicks==2){
+		 		
+		 		$(this).attr("src", $(this).data('reverseCard')); 
+		 		$('#background1 img').off('click', clicky);
+		 		//$('#background1 img').click(function() {
+
+				secondCard = $(this).data('reverseCard');
+				storeSecond = $(this); 
+				//console.log("second click");
+
+					if(firstCard==secondCard) { 
+						matches++;
+						console.log(matches);  	
+						$('#background1 img').on('click', clicky);										
+					} else {
+						//create a timer with a pause; 
+						setTimeout(function() {
+							storeImage.attr("src","img/tile-back.png");
+							storeSecond.attr("src","img/tile-back.png");
+							$('#background1 img').on('click', clicky);
+						}, 1000);
+					}
+				 
+				totalTries++; 
+				clicks=1;
+				console.log(totalTries +" tries"); 
+			} 
+				//when the user has 8 matches alert the user with a YAY and return how many tries 
+		if(matches==8) {
+			console.log("You won!" + "took you" + totalTries + "tries");
 		} 
-		clicks++;
+		} 
 
-		//do something if click is above 2 and checks if the images are the same
-		//if it is not the same then click resets to 0 and the images flip back
-
-	console.log(clicks); 
-	
-	});  
-
-
-//check if they match
-//increment match
-//keep images on screen 
-//resets back 
-//add timer 
+	}
 
 
 
-	//flips over first card that the user clicks 
-		//switch tiles that the user clicks on 
-	//if user clicks a second card then check if it is the same
-	//if it is the same it should remain on the screen 
+document.addEventListener('DOMContentLoaded', onReady);
 
 
 
 
-
-
-
+//when someone first clicks start timer 
+//starts game 
+//when matches is 8 show them 
+//console.log matches==1 
+//click function that you can call 
+//new game will call a function 
+//when click event happen 
 
 
 
